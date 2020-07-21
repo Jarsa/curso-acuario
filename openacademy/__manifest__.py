9e1f2e59c846082b86bd1a3bc03ed956a10fa37f
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-07-21 17:23:46 -0500

    [ADD] openacademy: add new module to the repository

---
+++ openacademy/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020, Jarsa
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    'name': 'OpenAcademy',
+    'summary': 'Manage trainings',
+    'version': '13.0.1.0.0',
+    'category': 'School',
+    'website': 'https://www.jarsa.com.mx',
+    'author': 'Jarsa, Odoo Community Association (OCA)',
+    'license': 'LGPL-3',
+    'depends': [],
+    'data': [],
+    'demo': [],
+}
