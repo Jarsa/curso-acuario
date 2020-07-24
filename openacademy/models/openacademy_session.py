56a161a6faacb0d630506fa1626796ba8e3f199a
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-07-24 17:02:05 -0500

    [ADD] openacademy: add onchange, constrains and modify copy method

--- openacademy/models/openacademy_session.py
+++ openacademy/models/openacademy_session.py
@@ -2,6 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class OpenacademySession(models.Model):
@@ -29,3 +30,27 @@ class OpenacademySession(models.Model):
                 rec.taken_seats = 0.0
             else:
                 rec.taken_seats = 100.0 * len(rec.attendee_ids) / rec.seats
+
+    @api.onchange('seats', 'attendee_ids')
+    def _verify_valid_seats(self):
+        if self.seats < 0:
+            return {
+                'warning': {
+                    'title': "Incorrect 'seats' value",
+                    'message': "The number of available seats may not be negative",
+                },
+            }
+        if self.seats < len(self.attendee_ids):
+            return {
+                'warning': {
+                    'title': "Too many attendees",
+                    'message': "Increase seats or remove excess attendees",
+                },
+            }
+
+    @api.constrains('instructor_id', 'attendee_ids')
+    def _check_instructor_not_in_attendees(self):
+        for rec in self:
+            if rec.instructor_id and rec.instructor_id in rec.attendee_ids:
+                raise ValidationError(
+                    "A session's instructor can't be an attendee")
